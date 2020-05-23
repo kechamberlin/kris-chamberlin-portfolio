a37c764c0c2a922f4576761c5e3bbb52baa5223b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-23 11:53:53 -0700

    Document format changed by Prettier extension.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Kris Chamberlin's Portfolio
-      </header>
+    <div className='App'>
+      <header className='App-header'>Kris Chamberlin's Portfolio</header>
     </div>
   );
 }
