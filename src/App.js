2fecc43281f2db3aef9fd48d04e57114bf6343ef
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-23 12:20:42 -0700

    Imported Navbar component and added to return statement.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <p>Kris Chamberlin's Portfolio</p>
       {/* <header className='App-header'>Kris Chamberlin's Portfolio</header> */}
     </div>
