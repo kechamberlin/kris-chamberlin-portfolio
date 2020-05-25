84dddb5b577f56a51a5c93a35494f18d9f49a67f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-25 11:28:31 -0700

    Imported Projects component and added it to return statement underneath About component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Navbar />
       <About />
+      <Projects />
       {/* <header className='App-header'>Kris Chamberlin's Portfolio</header> */}
     </div>
   );
