44d0f7d0cb6d4142055a1a409bad07f08c9480df
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-27 14:26:02 -0700

    Imported my-picture and added an img tag with the picture as the source.

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Me from '../images/my-picture.jpeg';
 
 function About() {
   return (
     <div className='about'>
       <p>About</p>
+      <img src={Me} alt='me with my coding certificate' />
     </div>
   );
 }
