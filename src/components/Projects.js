53760e4f8d218726349be9093b1317887091a9c3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-05-28 15:45:20 -0700

    Imported Card, Row, Col, and Container components from react-bootstrap. Added components to return statement.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 function Projects() {
   return (
     <div className='projects'>
       <p>Projects</p>
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <Card className='text-center'>
+              <Card.Img variant='top' src='holder.js/100px180' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Card.Link href='#'>Card Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={6}>
+            <Card className='text-center'>
+              <Card.Img variant='top' src='holder.js/100px180' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Card.Link href='#'>Card Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
